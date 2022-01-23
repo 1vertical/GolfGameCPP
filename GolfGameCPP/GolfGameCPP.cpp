b8f8988f4e11d456ad683c3acb1156526a0d8c0d
Shaun Smith <shortshaunsmith@hotmail.com>
2022-01-23 15:26:25 +0200

    Main

---
+++ GolfGameCPP/GolfGameCPP.cpp
@@ -0,0 +1,39 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to GolfGameCPP!\n";
+    cout << "This game is an exercise and simple game to play against an AI opponent on randomly generated courses.\n";
+    cout << "Commands will be given through the game to proceed in-game.";
+}
+
+class Game {
+
+public:
+
+    int iPlayerScore;
+    int iComputerScore;
+    int iWindFactor;
+    int iPowerFactor;
+    int iRandomInteger;
+    enum enmStick {
+        40 = 
+    };
+
+    void PlayGame() {
+
+    }
+
+    void QuitGame() {
+
+    }
+
+    void StrikeBall() {
+
+    }
+};
+
